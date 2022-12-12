5b10ebd4cbf8408b4f1be5266333283ab37dc0e1
Shane <mhurtado1@berklee.edu>
2022-12-12 10:08:36 -0600

    making-mugs

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const material of clay) {
+   const mug = `${material} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
